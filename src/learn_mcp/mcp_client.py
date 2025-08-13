efb509c49ceafbf304501207a9098d1a0555f67f
syedshahmirsultan <syedshahmirsultan@gmail.com>
2025-08-14 00:33:08 +0500

    Added Resources

--- src/learn_mcp/mcp_client.py
+++ src/learn_mcp/mcp_client.py
@@ -2,6 +2,7 @@ from mcp.client.streamable_http import streamablehttp_client
 from mcp import ClientSession
 from contextlib import AsyncExitStack
 import asyncio
+from pydantic import AnyUrl
 
 
 class MCPClient:
@@ -30,6 +31,24 @@ class MCPClient:
         print("Listing Tools")
         return (await self.session.list_tools()).tools
 
+    async def list_resources(self):
+        print("Listing Resources")
+        result = await self.session.list_resources()
+        return result.resources
+    
+
+    async def get_template_resource(self):
+        print("Getting Template Resource")
+        result = await self.session.list_resource_templates()
+        return result.resourceTemplates
+
+    async def read_resource(self,uri:str):
+        result = await self.session.read_resource(AnyUrl(uri))
+        print("Result:",result)
+        return result.contents[0].text
+
+
+
 
 async def main():
     async with MCPClient("http://127.0.0.1:8000/mcp") as client:
@@ -37,7 +56,15 @@ async def main():
         print("Tools:",tools)
         result = await client.call_tool("Add Two Nums",{"a":1,"b":2})
         print("Result:",result.content)
-
+        resources = await client.list_resources()
+        print("Resources:",resources)
+        template_resources = await client.get_template_resource()
+        resource  = template_resources[0].uriTemplate.replace("{doc_name}","intro")
+        print("Template Resources:",resource)
+        result = await client.read_resource(resource)
+        print("Template Resource Result:",result)
+        listOfDocs = await client.read_resource("docs://documents")
+        print("List of Docs:",listOfDocs)
 
 # Run the main function
 def run_main():
--- src/learn_mcp/server.py
+++ src/learn_mcp/server.py
@@ -15,3 +15,24 @@ def add_two_nums(a:int,b:int):
 def subtract_two_nums(a:int,b:int):
     print("Subtracting two nums !")
     return a-b
+
+
+docs ={
+    "intro":"Introduction To MCP",
+    "useCase":"This is a Use Case",
+    "Advantages":"This is an Advantage",
+    "Disadvantages":"This is a Disadvantage"
+}
+
+@mcp.resource("docs://documents",mime_type="application/json")
+def documents():
+    "Returns the Docs List"
+    print("Returning Docs List",list(docs.keys()))
+    return list(docs.keys())
+
+
+@mcp.resource("docs://documents/{doc_name}",mime_type="application/json")
+def document(doc_name:str):
+    "Returns the Doc Content"
+    print("Returning Doc Content for ",docs[doc_name])
+    return docs[doc_name]
