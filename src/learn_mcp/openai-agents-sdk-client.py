3dd4a40698a1467784523b50618c1f726e261c48
syedshahmirsultan <syedshahmirsultan@gmail.com>
2025-08-18 23:20:13 +0500

    Used OpenAI Agents SDK Client for MCP server

---
+++ src/learn_mcp/openai-agents-sdk-client.py
@@ -0,0 +1,46 @@
+from agents import Agent,Runner, OpenAIChatCompletionsModel
+from openai import AsyncOpenAI
+from agents.mcp import MCPServerStreamableHttpParams,MCPServerStreamableHttp
+from dotenv import load_dotenv
+import os
+import asyncio
+
+load_dotenv()
+
+mcp_url = "http://127.0.0.1:8000/mcp"
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+client = AsyncOpenAI(
+    api_key=GEMINI_API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+
+model = OpenAIChatCompletionsModel(
+    openai_client=client,
+    model="gemini-2.0-flash"
+)
+
+
+async def main():
+   mcp_param = MCPServerStreamableHttpParams(url=mcp_url)
+   async with MCPServerStreamableHttp(params=mcp_param) as mcp_server_client:
+    print("Tools List:",await mcp_server_client.list_tools())
+
+    agent = Agent(
+    name="MCP Agent",
+    model=model,
+    mcp_servers=[mcp_server_client]
+   )
+
+    result = await Runner.run(agent,"What is the mood of John?")
+    print("Result:",result)
+    return result
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        print(f"An unhandled error occurred in the agent script: {e}")
--- src/learn_mcp/server.py
+++ src/learn_mcp/server.py
@@ -7,12 +7,17 @@ mcp = FastMCP(name="Server 1", stateless_http=True)
 mcp_app = mcp.streamable_http_app()
 
 
-@mcp.tool(name="Add Two Nums",description="This tool Adds Two Numbers")
+@mcp.tool(name="add_two_nums",description="This tool Adds Two Numbers")
 def add_two_nums(a:int,b:int):
     print("Adding two nums !")
     return a+b
 
-@mcp.tool(name="Subtract Two Nums",description="This Subtracts Two Numbers")
+@mcp.tool()
+def get_mood(name:str):
+    print("Getting Modd")
+    return f'{name} is feeling good'
+
+@mcp.tool(name="subtract_two_nums",description="This Subtracts Two Numbers")
 def subtract_two_nums(a:int,b:int):
     print("Subtracting two nums !")
     return a-b
