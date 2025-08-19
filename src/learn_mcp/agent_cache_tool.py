5810c48b22bd7f37ec4d65b8e9476949f534d915
syedshahmirsultan <syedshahmirsultan@gmail.com>
2025-08-19 12:43:54 +0500

    Added cache_tools_list

--- src/learn_mcp/agent_cache_tool.py
+++ src/learn_mcp/agent_cache_tool.py
@@ -1,6 +1,6 @@
 from agents import Agent,Runner, OpenAIChatCompletionsModel
 from openai import AsyncOpenAI
-from agents.mcp import MCPServerStreamableHttpParams,MCPServerStreamableHttp,create_static_tool_filter
+from agents.mcp import MCPServerStreamableHttpParams,MCPServerStreamableHttp
 from dotenv import load_dotenv
 import os
 import asyncio
@@ -12,34 +12,29 @@ mcp_url = "http://127.0.0.1:8000/mcp"
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-
-#Reference: https://ai.google.dev/gemini-api/docs/openai
 client = AsyncOpenAI(
     api_key=GEMINI_API_KEY,
     base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
 )
-
 
 model = OpenAIChatCompletionsModel(
     openai_client=client,
     model="gemini-2.0-flash"
 )
 
-
 async def main():
-   mcp_param = MCPServerStreamableHttpParams(url=mcp_url)
-   async with MCPServerStreamableHttp(params=mcp_param) as mcp_server_client:
-    # print("Tools List:",await mcp_server_client.list_tools())
+    mcp_params = MCPServerStreamableHttpParams(url=mcp_url)
+    async with MCPServerStreamableHttp(params=mcp_params,cache_tools_list=True) as mcp_server_client:
+        print("Tools List:",await mcp_server_client.list_tools())
+        agent = Agent(
+            name="MCP Agent",
+            model=model,
+            mcp_servers=[mcp_server_client]
+        )
+        result = await Runner.run(agent,"What is the mood of John?")
+        print("Result:",result.final_output)
+        return result
 
-    agent = Agent(
-    name="MCP Agent",
-    model=model,
-    mcp_servers=[mcp_server_client]
-   )
-
-    result = await Runner.run(agent,"What is the mood of John?")
-    print("Result:",result)
-    return result
 
 if __name__ == "__main__":
     try:
